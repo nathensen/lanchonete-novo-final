07f77eeb0bdcada6753bce2bdd5a0f215c7ea85f
Nathensen <nanehensen@gmail.com>
2025-11-26 15:14:37 -0300

    forma de pagamento
    feito, pode ser melhorado

--- gestor-pedidos-lanchonete/sistema-de-gestao-de-pedidos/src/main/java/com/lanchonete/util/HistoricoTXT.java
+++ gestor-pedidos-lanchonete/sistema-de-gestao-de-pedidos/src/main/java/com/lanchonete/util/HistoricoTXT.java
@@ -11,21 +11,18 @@ import com.lanchonete.model.Vendedor;
 
 public class HistoricoTXT {
 
-    // Caminho do histórico
     private static final String CAMINHO = "historico/historico_pedidos.txt";
 
     public static void salvar(Pedido pedido, Vendedor vendedor) {
 
-        // Garantir que a pasta exista
+        // Criar pasta se não existir
         File pasta = new File("historico");
         if (!pasta.exists()) {
             pasta.mkdirs();
         }
 
-        // Print para ver onde o arquivo está sendo criado
-        System.out.println("Caminho real do arquivo: " + new File(CAMINHO).getAbsolutePath());
-
-        try (FileWriter writer = new FileWriter("historico_pedidos.txt")){
+        // Abrir arquivo no MODO APPEND (ACRESCENTAR)
+        try (FileWriter writer = new FileWriter(CAMINHO, true)) {
 
             writer.write("===== PEDIDO FINALIZADO =====\n");
             writer.write("Cliente: " + pedido.getNomeCliente() + "\n");
@@ -37,14 +34,15 @@ public class HistoricoTXT {
             for (var item : pedido.getItensConsumidos()) {
                 writer.write(" - " + item.descricao() + " | R$ " + String.format("%.2f", item.getPrecoVenda()) + "\n");
             }
+
             writer.write("-----------------------------\n\n");
 
-            // Confirmação no console
-            JOptionPane.showMessageDialog(null,"Pedido salvo no histórico: " + pedido.getNomeCliente());
+            JOptionPane.showMessageDialog(null,
+                "Pedido salvo no histórico: " + pedido.getNomeCliente());
 
         } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "Erro na gravacão do Arquivo"+e.getMessage());
-
+            JOptionPane.showMessageDialog(null,
+                "Erro ao gravar o arquivo: " + e.getMessage());
         }
     }
 }
