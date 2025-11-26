d7d05e6a1929c499d2749c09c5d4a3eeafb99fd7
Patrick-lab1 <patrick_campestrini@estudante.sesisenai.org.br>
2025-11-25 21:00:18 -0300

    Historico-Resolvido

--- gestor-pedidos-lanchonete/sistema-de-gestao-de-pedidos/src/main/java/com/lanchonete/util/HistoricoTXT.java
+++ gestor-pedidos-lanchonete/sistema-de-gestao-de-pedidos/src/main/java/com/lanchonete/util/HistoricoTXT.java
@@ -1,9 +1,10 @@
 package com.lanchonete.util;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+
+import javax.swing.JOptionPane;
 
 import com.lanchonete.model.Pedido;
 import com.lanchonete.model.Vendedor;
@@ -11,7 +12,7 @@ import com.lanchonete.model.Vendedor;
 public class HistoricoTXT {
 
     // Caminho do histórico
-    private static final String CAMINHO = "/historico/historico_pedidos.txt";
+    private static final String CAMINHO = "historico/historico_pedidos.txt";
 
     public static void salvar(Pedido pedido, Vendedor vendedor) {
 
@@ -24,26 +25,26 @@ public class HistoricoTXT {
         // Print para ver onde o arquivo está sendo criado
         System.out.println("Caminho real do arquivo: " + new File(CAMINHO).getAbsolutePath());
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(CAMINHO, true))) {
+        try (FileWriter writer = new FileWriter("historico_pedidos.txt")){
 
             writer.write("===== PEDIDO FINALIZADO =====\n");
             writer.write("Cliente: " + pedido.getNomeCliente() + "\n");
             writer.write("Vendedor: " + vendedor.getNome() + "\n");
             writer.write("Valor Total: R$ " + String.format("%.2f", pedido.calcularTotal()) + "\n");
+            writer.write("-----------------------------\n");
 
             writer.write("Itens:\n");
             for (var item : pedido.getItensConsumidos()) {
-                writer.write(" - " + item.descricao() +
-                             " | R$ " + String.format("%.2f", item.getPrecoVenda()) + "\n");
+                writer.write(" - " + item.descricao() + " | R$ " + String.format("%.2f", item.getPrecoVenda()) + "\n");
             }
-
             writer.write("-----------------------------\n\n");
 
             // Confirmação no console
-            System.out.println("Pedido salvo no histórico: " + pedido.getNomeCliente());
+            JOptionPane.showMessageDialog(null,"Pedido salvo no histórico: " + pedido.getNomeCliente());
 
         } catch (IOException e) {
-            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Erro na gravacão do Arquivo"+e.getMessage());
+
         }
     }
 }
