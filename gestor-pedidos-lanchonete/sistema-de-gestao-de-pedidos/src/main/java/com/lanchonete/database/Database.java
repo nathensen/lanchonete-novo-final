95804ba7d0b3209c33824f918fc604e0d7b7ad95
Nathensen <nanehensen@gmail.com>
2025-12-01 02:00:34 -0300

    database fase 1
    fase 1

---
+++ gestor-pedidos-lanchonete/sistema-de-gestao-de-pedidos/src/main/java/com/lanchonete/database/Database.java
@@ -0,0 +1,21 @@
+package com.lanchonete.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/lanchonete";
+    private static final String USER = "ADMEQUIPE";
+    private static final String PASSWORD = "1234";
+
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+}
